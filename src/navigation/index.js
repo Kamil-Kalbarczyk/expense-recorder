738216c693776eb5e4e7c60eb4a2f1625a96af07
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-03-30 06:49:22 +0200

    clear unnecessary comments from navigation component

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,20 +1,8 @@
+import { ListItems } from "./listItems";
 import { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import SettingsIcon from "@mui/icons-material/Settings";
-import LogoutIcon from "@mui/icons-material/Logout";
-import { Typography } from "@mui/material";
-import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-
-import { ListItems } from "./listItems";
 
 export const Navigation = () => {
   const [navigationState, setNavigationState] = useState(false);
@@ -22,48 +10,6 @@ export const Navigation = () => {
   const toggleDrawer = (anchor, open) => (event) => {
     setNavigationState(open);
   };
-
-  // const list = (anchor) => (
-  //   <Box
-  //     sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-  //     role="presentation"
-  //     onClick={toggleDrawer(anchor, false)}
-  //     onKeyDown={toggleDrawer(anchor, false)}
-  //   >
-  //     <Typography
-  //       variant="body1"
-  //       align="center"
-  //       sx={{ fontWeight: "bold", pt: "10px" }}
-  //     >
-  //       Recorded expenses:
-  //     </Typography>
-  //     <List>
-  //       {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-  //         <ListItem key={text} disablePadding>
-  //           <ListItemButton>
-  //             <ListItemIcon>
-  //               <AttachMoneyIcon />
-  //             </ListItemIcon>
-  //             <ListItemText primary={text} />
-  //           </ListItemButton>
-  //         </ListItem>
-  //       ))}
-  //     </List>
-  //     <Divider />
-  //     <List>
-  //       {["All mail", "Trash", "Spam"].map((text, index) => (
-  //         <ListItem key={text} disablePadding>
-  //           <ListItemButton>
-  //             <ListItemIcon>
-  //               {index % 2 === 0 ? <SettingsIcon /> : <LogoutIcon />}
-  //             </ListItemIcon>
-  //             <ListItemText primary={text} />
-  //           </ListItemButton>
-  //         </ListItem>
-  //       ))}
-  //     </List>
-  //   </Box>
-  // );
 
   return (
     <div>
@@ -79,7 +25,6 @@ export const Navigation = () => {
         open={navigationState}
         onClose={toggleDrawer("left", false)}
       >
-        {/* {list("left")} */}
         <ListItems anchor={"left"} toggleDrawer={toggleDrawer} />
       </Drawer>
     </div>
