975b84913cb9f9335a7915f0ac329197c869c706
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-03-30 06:54:13 +0200

    get projects name from useContext and render them in navigation buttons

--- src/navigation/listItems/index.js
+++ src/navigation/listItems/index.js
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { ProjectsContext } from "../../contexts/projects/ProjectsContext";
+
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
@@ -10,44 +13,48 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 
-export const ListItems = ({ anchor, toggleDrawer }) => (
-  <Box
-    sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-    role="presentation"
-    onClick={toggleDrawer(anchor, false)}
-    onKeyDown={toggleDrawer(anchor, false)}
-  >
-    <Typography
-      variant="body1"
-      align="center"
-      sx={{ fontWeight: "bold", pt: "10px" }}
+export const ListItems = ({ anchor, toggleDrawer }) => {
+  const projects = useContext(ProjectsContext).projects;
+
+  return (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
     >
-      Recorded expenses:
-    </Typography>
-    <List>
-      {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-        <ListItem key={text} disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <AttachMoneyIcon />
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-    <Divider />
-    <List>
-      {["All mail", "Trash", "Spam"].map((text, index) => (
-        <ListItem key={text} disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              {index % 2 === 0 ? <SettingsIcon /> : <LogoutIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-  </Box>
-);
+      <Typography
+        variant="body1"
+        align="center"
+        sx={{ fontWeight: "bold", pt: "10px" }}
+      >
+        Recorded expenses:
+      </Typography>
+      <List>
+        {projects.map((project, index) => (
+          <ListItem key={project.project_id} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <AttachMoneyIcon />
+              </ListItemIcon>
+              <ListItemText primary={project.project_name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <SettingsIcon /> : <LogoutIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
