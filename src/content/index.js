ce040cac32570d11ea39f8d80a87bb7490aaf059
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-03-30 06:38:07 +0200

    add fetch project into ProjectContext

--- src/content/index.js
+++ src/content/index.js
@@ -1,19 +1,22 @@
 import { Navigation } from "../navigation";
 import { GridExpenses } from "./expenseGrid";
 import { Login } from "../login";
-import { CheckProjectExists } from "../common/checkProjectExists";
+import { ProjectsContextProvider } from "../contexts/projects/ProjectsContext";
+// import { CheckProjectExists } from "../common/checkProjectExists";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/auth/AuthContext";
+import { ProjectsContext } from "../contexts/projects/ProjectsContext";
 
 export const Content = () => {
   const isAuthorization = useContext(AuthContext);
+  const projects = useContext(ProjectsContext);
   if (isAuthorization.authorization) {
     return (
-      <>
+      <ProjectsContextProvider>
         <Navigation />
-        <CheckProjectExists />
+        {/* <CheckProjectExists /> */}
         <GridExpenses />
-      </>
+      </ProjectsContextProvider>
     );
   } else {
     return <Login />;
---
+++ src/contexts/projects/ProjectsContext.js
@@ -0,0 +1,61 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import { AuthContext } from "../auth/AuthContext";
+import { app } from "../../firebase";
+import {
+  getFirestore,
+  collection,
+  //   addDoc,
+  query,
+  where,
+  getDocs,
+  orderBy,
+} from "firebase/firestore";
+
+const db = getFirestore(app);
+
+export const ProjectsContext = createContext(null);
+
+export const ProjectsContextProvider = ({ children }) => {
+  const isAuthorization = useContext(AuthContext);
+  const [projects, setProjects] = useState(null);
+  const userID = isAuthorization.authorization.uid;
+
+  const convertTimestampToDate = (timestamp) =>
+    new Date(timestamp * 1000).toISOString().slice(0, 10).replace("T", " ");
+
+  const getProject = async () => {
+    const q = query(
+      collection(db, "projects"),
+      where("userID", "==", userID),
+      orderBy("create_date")
+    );
+
+    const querySnapshot = await getDocs(q);
+    setProjects(
+      querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        project_id: doc.id,
+        period_from: convertTimestampToDate(doc.data()?.period_from?.seconds),
+        period_to: convertTimestampToDate(doc.data()?.period_to?.seconds),
+      }))
+    );
+
+    console.log(
+      querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        project_id: doc.id,
+        period_from: convertTimestampToDate(doc.data()?.period_from?.seconds),
+        period_to: convertTimestampToDate(doc.data()?.period_to?.seconds),
+      }))
+    );
+  };
+  useEffect(() => {
+    getProject();
+  }, [userID]);
+
+  return (
+    <ProjectsContext.Provider value={{ projects, setProjects }}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
