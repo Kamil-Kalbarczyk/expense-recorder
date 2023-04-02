f34ac23006d22ef72b3eddcf3a6b68df6025d04c
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-04-02 07:25:52 +0200

    add categories and colums from data base new structure

--- src/content/expenseGrid/index.js
+++ src/content/expenseGrid/index.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
+import { AuthContext } from "../../contexts/auth/AuthContext";
 import { app } from "../../firebase";
 import {
   getFirestore,
@@ -16,19 +17,52 @@ import { DataGrid } from "@mui/x-data-grid";
 const db = getFirestore(app);
 
 export const GridExpenses = () => {
+  const isAuthorization = useContext(AuthContext);
+  const userID = isAuthorization.authorization.uid;
   const { projectID } = useParams();
   const [dataGrid, setDataGrid] = useState([]);
+  const [columnsGrid, setColumnsGrid] = useState([]);
   const [loading, setLoading] = useState(true);
 
   // =============== Fetching data start ===============
-  const getDataGridQuery = query(
+
+  // columns
+  const getColumnsGridQuery = query(
+    collection(db, "categories"),
+    where("userID", "==", userID),
+    where("projects", "array-contains", projectID),
+    orderBy("create_date", "asc")
+  );
+
+  const getColumnsGridFromFirestore = async () => {
+    const querySnapshot = await getDocs(getColumnsGridQuery);
+    const columnsFromFirestore = [];
+    querySnapshot.forEach((doc) => {
+      columnsFromFirestore.push({
+        id: doc.id,
+        ...doc.data(),
+        create_date: convertTimestampToDate(doc.data()?.create_date?.seconds),
+      });
+    });
+    // set columns for grid
+    setColumnsGrid(
+      columnsFromFirestore.map((item) => ({
+        id: item.id,
+        category: item.category,
+      }))
+    );
+  };
+
+  // rows
+  const getRowsGridQuery = query(
     collection(db, "expenses_rows"),
     where("project_id", "==", projectID),
+    where("userID", "==", userID),
     orderBy("create_date", "asc")
   );
 
   const getDataGridFromFirestore = async () => {
-    const querySnapshot = await getDocs(getDataGridQuery);
+    const querySnapshot = await getDocs(getRowsGridQuery);
     const dataFromFirestore = [];
     querySnapshot.forEach((doc) => {
       dataFromFirestore.push({
@@ -37,7 +71,6 @@ export const GridExpenses = () => {
         create_date: convertTimestampToDate(doc.data()?.create_date?.seconds),
       });
     });
-
     // set data for grid
     setDataGrid(dataFromFirestore);
 
@@ -45,39 +78,35 @@ export const GridExpenses = () => {
     setLoading(false);
   };
 
+  const fetchDataFromFirestore = async () => {
+    await getColumnsGridFromFirestore();
+    await getDataGridFromFirestore();
+  };
+
   useEffect(() => {
-    getDataGridFromFirestore();
+    fetchDataFromFirestore();
   }, [projectID]);
 
   // =============== Fetching data end ===============
 
   // =============== Building grid start ===============
-  const allCategories = dataGrid.map((data) => Object.keys(data.expenses));
-  const categoriesTogether = ["id"];
-  allCategories.forEach((categoryArray) => {
-    categoryArray.forEach((singleCategory) => {
-      categoriesTogether.push(singleCategory);
-    });
-  });
-  const categories = [...new Set(categoriesTogether)];
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 150,
+      hidden: true,
+      // description: "This column has a value getter and is not sortable.",
+    },
+  ];
 
-  const columns = categories.map((column) => {
-    if (column === "id") {
-      return {
-        field: column,
-        headerName: column,
-        width: 150,
-        hidden: true,
-        // description: "This column has a value getter and is not sortable.",
-      };
-    } else {
-      return {
-        field: column,
-        headerName: column,
-        width: 150,
-        editable: true,
-      };
-    }
+  columnsGrid.forEach((column) => {
+    columns.push({
+      field: column.id,
+      headerName: column.category,
+      width: 150,
+      hidden: true,
+    });
   });
 
   const rows = dataGrid.map((row) => {
@@ -86,6 +115,7 @@ export const GridExpenses = () => {
       ...row.expenses,
     };
   });
+
   // =============== Building grid end ===============
   return (
     <Box sx={{ height: 400, width: "100%" }}>
