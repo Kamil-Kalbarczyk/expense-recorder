79482498ab142014e4fe0ce994f401f3f0a52fd9
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-04-12 06:23:23 +0200

    create basic form for add new recorder

--- src/content/main/newRecorder/index.js
+++ src/content/main/newRecorder/index.js
@@ -1,3 +1,64 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
+
 export const NewRecorder = () => {
-  return <h1>Set new recorder</h1>;
+  return (
+    // <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Create new expense recorder
+        </Typography>
+        <Box
+          component="form"
+          // onSubmit={handleSubmit}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="name"
+            name="name"
+            // autoComplete="name"
+            autoFocus
+            // onChange={handleChange}
+            // value={name}
+          />
+          <Button
+            type="submit"
+            // fullWidth
+            variant="contained"
+            // color="success"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Create
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+    // </ThemeProvider>
+  );
 };
