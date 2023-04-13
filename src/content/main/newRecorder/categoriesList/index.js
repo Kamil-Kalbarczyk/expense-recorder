104e2baaf54d986f47f7c94fedc6e480dae28a21
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-04-13 04:55:32 +0200

    add setting icon into category list - create new recorder

--- src/content/main/newRecorder/categoriesList/index.js
+++ src/content/main/newRecorder/categoriesList/index.js
@@ -1,34 +1,57 @@
 import { List } from "@mui/material";
 import { ListItem } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
-// import CommentIcon from "@mui/material/CommentIcon";
 import ListItemText from "@mui/material/ListItemText";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { Checkbox } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export const CategoriesList = () => {
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {[1, 2, 3].map((value) => (
-        <ListItem
-          key={value}
-          disableGutters
-          secondaryAction={
-            // <IconButton aria-label="comment">
-            //   <SettingsIcon />
-            // </IconButton>
-            <Checkbox
-              //   value="checkedA"
-              //   inputProps={{
-              //     "aria-label": "Checkbox A",
-              //   }}
-              defaultChecked
-            />
-          }
-        >
-          <ListItemText primary={`Line item ${value}`} />
-        </ListItem>
-      ))}
-    </List>
+    <Box>
+      <Typography
+        sx={{ textAlign: "center", mt: 1, padding: 0 }}
+        variant="subtitle1"
+      >
+        Categories:
+      </Typography>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          margin: 0,
+          padding: 0,
+        }}
+      >
+        {[1, 2, 3].map((value) => (
+          <ListItem
+            key={value}
+            disableGutters
+            secondaryAction={
+              <div>
+                <IconButton
+                  aria-label="settings"
+                  onClick={(e) => {
+                    console.log(e);
+                  }}
+                >
+                  <SettingsIcon />
+                </IconButton>
+                <Checkbox
+                  onChange={(e) => {
+                    console.log(e.target.checked);
+                  }}
+                  defaultChecked
+                />
+              </div>
+            }
+          >
+            <ListItemText primary={`Line item ${value}`} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 };
