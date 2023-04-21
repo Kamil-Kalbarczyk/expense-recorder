cd356a347088050c95a91173e5aed12c219bc5e0
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-04-21 07:00:20 +0200

    create home page

--- src/content/main/index.js
+++ src/content/main/index.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Home } from "./home";
 import { GridExpenses } from "./expenseGrid";
 import { NewRecorder } from "./newRecorder";
 import { LogOut } from "./logOut";
@@ -6,7 +7,7 @@ import { LogOut } from "./logOut";
 export const Main = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>Main Home</h1>} />
+      <Route path="/" element={<Home />} />
       <Route path="/grid/:projectID" element={<GridExpenses />} />
       <Route path="/new-recorder" element={<NewRecorder />} />
       <Route path="/log-out" element={<LogOut />} />
