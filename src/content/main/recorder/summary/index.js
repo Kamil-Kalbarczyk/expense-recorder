c8a1eb1d3583a545afdc300a21d84c6b5de19924
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-04-24 05:58:34 +0200

    add grid into summary component

--- src/content/main/recorder/summary/index.js
+++ src/content/main/recorder/summary/index.js
@@ -9,21 +9,40 @@ export const Summary = (
   loading,
   setLoading,
   projectID,
-  userID
-  //   getDataGridFromFirestore
+  userID,
+  getDataGridFromFirestore
 ) => {
-  console.log(
-    dataGrid,
-    setDataGrid,
-    columnsGrid,
-    setColumnsGrid,
-    loading,
-    setLoading,
-    projectID,
-    userID
-  );
-
-  const columns = [{ field: "id", headerName: "Context", width: 150 }];
+  const columns = [
+    { field: "id", headerName: "Context", width: 150 },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
 
   const rows = [
     { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
